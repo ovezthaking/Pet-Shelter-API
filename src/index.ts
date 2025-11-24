59733926e388931ce38c08b4af3c02c30f94eac1
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-24 22:40:51 +0100

    feat: define pet type and return pet object in root route

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,23 @@ const PORT = 8000
 
 const app: Express = express()
 
+
+type petType = {
+    name: string,
+    species: string,
+    adopted: boolean,
+    age: number
+}
+
+const pet: petType = {
+    name: "Dogens",
+    species: "no species",
+    adopted: false,
+    age: 1
+}
+
 app.get('/', (req, res) => {
-    res.json()
+    res.json(pet)
 })
 
 app.listen(PORT, ():void => {
