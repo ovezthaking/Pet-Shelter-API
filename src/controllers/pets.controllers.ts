c37d1ade1fa35e5ac8f7d4a98f0133b0e82e8235
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-25 19:19:38 +0100

    feat: move pet retrieval logic to controllers and simplify routes

---
+++ src/controllers/pets.controllers.ts
@@ -0,0 +1,59 @@
+import type { Request, Response } from 'express'
+import type { Pet } from '../data/pets'
+import { pets } from '../data/pets'
+
+
+type PetQueryParams = {
+    species?: string,
+    adopted?: 'true' | 'false',
+    minAge?: string,
+    maxAge?: string
+}
+
+export const getPets = (
+    req:Request<{}, unknown, {}, PetQueryParams>, 
+    res:Response<Pet[]>
+):void=> {
+
+  const { species, adopted, minAge, maxAge } = req.query
+
+  let filteredPets: Pet[] = pets
+
+  if (species){
+    filteredPets = filteredPets.filter((pet: Pet): boolean => 
+        pet.species.toLowerCase() === species.toLowerCase())
+  }
+
+  if (adopted){
+    filteredPets = filteredPets.filter((pet: Pet): boolean => 
+        pet.adopted === JSON.parse(adopted))
+  }
+
+  if (maxAge){
+    filteredPets = filteredPets.filter((pet: Pet): boolean =>
+        pet.age <= JSON.parse(maxAge))
+  }
+
+  if (minAge){
+    filteredPets = filteredPets.filter((pet: Pet): boolean =>
+        pet.age >= JSON.parse(minAge))
+  }
+
+  res.json(filteredPets)
+}
+
+export const getPetById = (
+    req: Request<{id: string}>, 
+    res: Response<Pet | {error: string}>
+): void | Response<{error: string}> => {
+
+    const id: number = parseInt(req.params.id, 10)
+
+    const pet: Pet | undefined = pets.find((pet: Pet): boolean => pet.id === id)
+
+    if (!pet){
+        return res.status(400).json({error: 'Pet not found by ID'})
+    }
+
+    res.status(200).json(pet)
+}
