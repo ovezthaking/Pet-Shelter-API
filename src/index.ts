1b0f709b90d399f0e80c5e5004804f56d6cca805
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-25 02:07:50 +0100

    feat: export Pet type and update root route response type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import type { Express, Request, Response } from 'express'
 import { pets } from './data/pets'
+import type { Pet } from './data/pets'
 import cors from 'cors'
 
 const PORT = 8000
@@ -10,11 +11,11 @@ const app: Express = express()
 
 app.use(cors())
 
-app.get('/', (req: Request, res: Response): void => {
-    res.json({pets: pets})
+app.get('/', (req: Request, res: Response<Pet[]>): void => {
+    res.json(pets)
 })
 
-app.use((req: Request, res: Response): void => {
+app.use((req: Request, res: Response<{error: string}>): void => {
     res.status(404).json({error: 'Endpoint not found'})
 })
 
