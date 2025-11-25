a0a0455bdf52319c3be267afcda3b0838f09b8f0
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-25 19:09:04 +0100

    feat: implement pet filtering and retrieval endpoints with age parameters, refactor

--- src/index.ts
+++ src/index.ts
@@ -11,49 +11,6 @@ const app: Express = express()
 
 app.use(cors())
 
-type PetQueryParams = {
-    species?: string,
-    adopted?: 'true' | 'false'
-}
-
-app.get('/', (
-    req:Request<{}, unknown, {}, PetQueryParams>, 
-    res:Response<Pet[]>
-):void=> {
-
-  const { species, adopted } = req.query
-
-  let filteredPets: Pet[] = pets
-
-  if(species){
-    filteredPets = filteredPets.filter((pet: Pet): boolean => 
-        pet.species.toLowerCase() === species.toLowerCase())
-  }
-
-  if(adopted){
-    filteredPets = filteredPets.filter((pet: Pet): boolean => 
-        pet.adopted === JSON.parse(adopted))
-  }
-
-  res.json(filteredPets)
-})
-
-app.get('/:id', (
-    req: Request<{id: string}>, 
-    res: Response<Pet | {error: string}>
-): void | Response<{error: string}> => {
-
-    const id: number = parseInt(req.params.id, 10)
-
-    const pet: Pet | undefined = pets.find((pet: Pet): boolean => pet.id === id)
-
-    if (!pet){
-        return res.status(400).json({error: 'Pet not found by ID'})
-    }
-
-    res.status(200).json(pet)
-})
-
 app.use((req: Request, res: Response<{error: string}>): void => {
     res.status(404).json({error: 'Endpoint not found'})
 })
---
+++ src/routes/pets.routes.ts
@@ -0,0 +1,61 @@
+import express from 'express'
+import type { Router, Request, Response } from 'express'
+import type { Pet } from '../data/pets'
+import { pets } from '../data/pets'
+
+const petRouter: Router = express.Router()
+
+type PetQueryParams = {
+    species?: string,
+    adopted?: 'true' | 'false',
+    minAge?: string,
+    maxAge?: string
+}
+
+petRouter.get('/', (
+    req:Request<{}, unknown, {}, PetQueryParams>, 
+    res:Response<Pet[]>
+):void=> {
+
+  const { species, adopted, minAge, maxAge } = req.query
+
+  let filteredPets: Pet[] = pets
+
+  if (species){
+    filteredPets = filteredPets.filter((pet: Pet): boolean => 
+        pet.species.toLowerCase() === species.toLowerCase())
+  }
+
+  if (adopted){
+    filteredPets = filteredPets.filter((pet: Pet): boolean => 
+        pet.adopted === JSON.parse(adopted))
+  }
+
+  if (maxAge){
+    filteredPets = filteredPets.filter((pet: Pet): boolean =>
+        pet.age <= JSON.parse(maxAge))
+  }
+
+  if (minAge){
+    filteredPets = filteredPets.filter((pet: Pet): boolean =>
+        pet.age >= JSON.parse(minAge))
+  }
+
+  res.json(filteredPets)
+})
+
+petRouter.get('/:id', (
+    req: Request<{id: string}>, 
+    res: Response<Pet | {error: string}>
+): void | Response<{error: string}> => {
+
+    const id: number = parseInt(req.params.id, 10)
+
+    const pet: Pet | undefined = pets.find((pet: Pet): boolean => pet.id === id)
+
+    if (!pet){
+        return res.status(400).json({error: 'Pet not found by ID'})
+    }
+
+    res.status(200).json(pet)
+})
