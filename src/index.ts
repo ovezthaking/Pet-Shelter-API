dd82998d7ca731a3dba2e38e4ccbcb2c560f49c8
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-25 02:01:11 +0100

    feat: add 404 error handling for undefined endpoints

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,10 @@ app.get('/', (req: Request, res: Response): void => {
     res.json({pets: pets})
 })
 
+app.use((req: Request, res: Response): void => {
+    res.status(404).json({error: 'Endpoint not found'})
+})
+
 app.listen(PORT, ():void => {
     console.log(`Listening on ${PORT}`)
 })
