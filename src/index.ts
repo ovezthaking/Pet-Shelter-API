38246a00fa246279898168caebe56b32951dc955
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-25 01:58:19 +0100

    feat: add CORS support and update package dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import type { Express, Request, Response } from 'express'
 import { pets } from './data/pets'
+import cors from 'cors'
 
 const PORT = 8000
 
 const app: Express = express()
 
+
+app.use(cors())
 
 app.get('/', (req: Request, res: Response): void => {
     res.json({pets: pets})
