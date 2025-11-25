db655249da309c9380541ecef95b0cbc0887db87
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-25 01:18:46 +0100

    feat: refactor pet data structure and move to separate file

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,11 @@
 import express from 'express'
 import type { Express } from 'express'
+import { pets } from './data/pets'
 
 const PORT = 8000
 
 const app: Express = express()
 
-
-type petType = {
-    name: string,
-    species: string,
-    adopted: boolean,
-    age: number
-}
-
-const pets: petType[] = [
-    {
-    name: "Dogens",
-    species: "Dog",
-    adopted: false,
-    age: 1
-    },
-    {
-    name: "Catens",
-    species: "Cat",
-    adopted: true,
-    age: 0.5
-    },
-]
 
 app.get('/', (req, res) => {
     res.json({pets: pets})
